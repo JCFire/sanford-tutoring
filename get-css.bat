31721a3904eb66b89313f08b1ed167f0a7f88999
JCFire <JCFire@users.noreply.github.com>
2019-05-20 23:51:14 -0400

    All done except services at small screen sizes

--- get-css.bat
+++
@@ -1,3 +0,0 @@
-call purgecss --css vendor/bootstrap/css/bootstrap.css --content src/*.html -o ./ --keyframes true --fontFace true
-call cssnano bootstrap.css ./src/styles/bootstrap.min.css --discardComments.removeAll
-del bootstrap.css
